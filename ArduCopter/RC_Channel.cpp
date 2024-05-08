836535e33324d0f211b96c84b0426f24293bb8c0
bnsgeyer <bnsgeyer@aol.com>
2024-05-07 22:23:24 -0400

    Copter: Add user aux to fail motor

--- ArduCopter/RC_Channel.cpp
+++ ArduCopter/RC_Channel.cpp
@@ -474,6 +474,17 @@ bool RC_Channel_Copter::do_aux_function(const aux_func_t ch_option, const AuxSwi
 
         case AUX_FUNC::USER_FUNC2:
             copter.userhook_auxSwitch2(ch_flag);
+            switch (ch_flag) {
+                case AuxSwitchPos::HIGH:
+                case AuxSwitchPos::MIDDLE:
+                    copter.motors->set_fail_motor_flag(true);
+                    gcs().send_text(MAV_SEVERITY_INFO, "motor failed");
+                    break;
+                default:
+                    copter.motors->set_fail_motor_flag(false);
+                    gcs().send_text(MAV_SEVERITY_INFO, "motor restored");
+                    break;
+                }
             break;
 
         case AUX_FUNC::USER_FUNC3:
